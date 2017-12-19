b0b74421b33c8d3757dfced5c626a5c8ddc0bfa9
Cristhian Fernandez <crisfervil@gmail.com>
2017-12-19 18:27:16 +0000

    Added window initialization

--- src/FakeXrmContext.ts
+++ src/FakeXrmContext.ts
@@ -1,4 +1,11 @@
 class FakeXrmContext {
+
+	constructor(window?:any){
+		if(window){
+			window.Xrm = this;
+		}
+	}
+
 	// https://msdn.microsoft.com/en-us/library/gg328474.aspx#BKK_XrmPageObjects
     public Page = new Page();
     public Utility = new CRMUtility();
